27f5ad6889239112e07e8294644516dec3e3c794
MathiasSeguy-Android2EE <mathias.seguy@android2ee.com>
2019-06-05 17:39:44 +0200

    First commit after migration of may 2019

--- app/src/main/java/com/android2ee/bug/animatedvectordrawableandlevellist/MainActivity.java
+++ app/src/main/java/com/android2ee/bug/animatedvectordrawableandlevellist/MainActivity.java
@@ -1,6 +1,6 @@
 package com.android2ee.bug.animatedvectordrawableandlevellist;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
